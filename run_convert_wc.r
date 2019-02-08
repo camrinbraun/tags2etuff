c27cdb0a2f26c5b23151affb62e5b49e52264bf6
camrinbraun <camrin.braun@gmail.com>
2019-02-07 18:17:45 -0700

    add MTI

--- run_convert_wc.r
+++ run_convert_wc.r
@@ -82,7 +82,27 @@ rm(fe)
 
 
 
+mti <- gdata::read.xls(paste(ptt, '_HR.xls', sep=''), sheet='Archival Data', skip=2,
+                       colClasses = c(rep(NA,7), rep('NULL', 4)),
+                       header=F)
+names(mti) <- c('DateTime', 'tempval','pressureval','lightval','temperature','depth','light')
+mti$DateTime <- as.POSIXct(mti$DateTime, format=HMMoce::findDateFormat(mti$DateTime), tz='UTC')
 
+# summarize with melt
+mti.new <- reshape2::melt(mti, id.vars=c('DateTime'), measure.vars = c('temperature','depth','light'))
+mti.new$VariableName <- mti.new$variable
+
+# merge with obs types and do some formatting
+mti.new <- merge(x = mti.new, y = obsTypes[ , c("VariableID","VariableName", 'VariableUnits')], by = "VariableName", all.x=TRUE)
+mti.new <- mti.new[,c('DateTime','VariableID','value','VariableName','VariableUnits')]
+names(mti.new) <- c('DateTime','VariableID','VariableValue','VariableName','VariableUnits')
+mti.new <- mti.new[order(mti.new$DateTime, mti.new$VariableID),]
+#mti.new <- mti.new[which(!is.na(mti.new$VariableValue)),]
+mti.new$DateTime <- as.POSIXct(mti.new$DateTime, tz='UTC')
+mti.new$DateTime <- format(mti.new$DateTime, '%Y-%m-%d %H:%M:%S') # yyyy-mm-dd hh:mm:ss
+
+# convert to positive depth values
+mti.new$VariableValue[which(mti.new$VariableName=='depth')] <- abs(mti.new$VariableValue[which(mti.new$VariableName=='depth')])
 
 #------------------------
 ## given a PSAT directory:
