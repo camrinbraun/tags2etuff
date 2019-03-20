f57cca6afe51de9b9ec335b94c89f311be753699
camrinbraun <camrin.braun@gmail.com>
2019-03-19 21:22:55 -0600

    add write out plot

--- R/qc_spot_etuff.r
+++ R/qc_spot_etuff.r
@@ -7,9 +7,7 @@
 #'
 
 
-qc_spot_etuff <- function(etuff, meta_row, writePDF = FALSE){
-
-  ## check appropriate sign on start/end coords
+qc_spot_etuff <- function(etuff, meta_row, writePDF = FALSE, cutdates = FALSE){
 
   ## any where datetime and variablevalue are identical?
 
@@ -17,13 +15,54 @@ qc_spot_etuff <- function(etuff, meta_row, writePDF = FALSE){
   df <- etuff %>% select(-c(VariableID, VariableUnits)) %>% spread(VariableName, VariableValue)
   df$latitude <- as.numeric(df$latitude)
   df$longitude <- as.numeric(df$longitude)
+  df$DateTime <- as.POSIXct(df$DateTime, tz='UTC')
   xl <- c(min(df$longitude), max(df$longitude))
   yl <- c(min(df$latitude), max(df$latitude))
+
+  ## check appropriate sign on start/end and track coords
+  ## just send warning as possible to have diff
+  if (any(sign(df$latitude) != sign(meta_row$geospatial_lat_start) |
+          sign(df$latitude) != sign(meta_row$geospatial_lat_end) |
+          sign(meta_row$geospatial_lat_end) != sign(meta_row$geospatial_lat_start))){
+    warning('Check latitudes.')
+    print(paste('Lat start', meta_row$geospatial_lat_start, '.'))
+    print(paste('Lat end', meta_row$geospatial_lat_end, '.'))
+    print(paste('Sign of track lat is', unique(sign(df$latitude)), '.'))
+  }
+
+  if (any(sign(df$longitude) != sign(meta_row$geospatial_lon_start) |
+          sign(df$longitude) != sign(meta_row$geospatial_lon_end) |
+          sign(meta_row$geospatial_lon_end) != sign(meta_row$geospatial_lon_start))){
+    warning('Check longitudes.')
+    print(paste('Lon start', meta_row$geospatial_lon_start, '.'))
+    print(paste('Lon end', meta_row$geospatial_lon_end, '.'))
+    print(paste('Sign of track lon is', unique(sign(df$longitude)), '.'))
+  }
+
+  ## if decide to deal with cutdates...
+  ## not working yet but easily made so
+  if (cutdates){
+    cutdates <- read.table('~/work/RCode/eddies/raw/cut_dates.csv', sep=',',header=T)
+    cutdates$rm_date1 <- parse_date_time(cutdates$rm_date1, orders = 'mdy HM')
+    cutdates$rm_date2 <- parse_date_time(cutdates$rm_date2, orders = 'mdy HM')
+
+    if(any(argos$ptt[1] %in% cutdates$ptt)){
+      cut.idx <- which(cutdates$ptt %in% argos$ptt[1])
+      for (b in 1:length(cut.idx)){
+        argos <- argos[which(argos$dt < cutdates$rm_date1[cut.idx[b]] | argos$dt > cutdates$rm_date2[cut.idx[b]]),]
+      }
+    }
+  }
+
+
+  #==========
+  ## BUILD PLOT
+  #==========
 
   ## get world map data
   world <- map_data('world')
 
-  if (writePDF) pdf(paste(meta_row$instrument_name, '-ggmap.pdf', sep=''), width=8, height=12)
+  #if (writePDF) pdf(paste(meta_row$instrument_name, '-ggmap.pdf', sep=''), width=8, height=12)
   p1 <- ggplot() + geom_polygon(data = world, aes(x=long, y = lat, group = group)) +
     coord_fixed(xlim=xl, ylim=yl, ratio=1.3) + xlab('') + ylab('') +
     geom_path(data = df, aes(x = longitude, y = latitude)) +
@@ -33,13 +72,23 @@ qc_spot_etuff <- function(etuff, meta_row, writePDF = FALSE){
     ggtitle(paste(meta_row$instrument_name))
 
   p2 <- ggplot(df, aes(y = latitude, x = DateTime, colour = DateTime)) + geom_point() + geom_path() +
-    geom_point(data = iniloc, aes(x = DateTime, y = lat), colour = c('green','red'), fill = c('green','red'), shape = 24)
+    geom_point(data = meta_row, aes(x = time_coverage_start, y = geospatial_lat_start), colour = c('green'), fill = c('green'), shape = 24) +
+    geom_point(data = meta_row, aes(x = time_coverage_end, y = geospatial_lat_end), colour = c('red'), fill = c('red'), shape = 24)
 
   p3 <- ggplot(df, aes(y = longitude, x = DateTime, colour = DateTime)) + geom_point() + geom_path() +
-    geom_point(data = iniloc, aes(x = DateTime, y = lon), colour = c('green','red'), fill = c('green','red'), shape = 24)
+    geom_point(data = meta_row, aes(x = time_coverage_start, y = geospatial_lon_start), colour = c('green'), fill = c('green'), shape = 24) +
+    geom_point(data = meta_row, aes(x = time_coverage_end, y = geospatial_lon_end), colour = c('red'), fill = c('red'), shape = 24)
 
-  grid.arrange(grobs = list(p1, p2, p3), ncol=1, heights = c(6,2,2))
+
   #layout_matrix = rbind(c(1), c(2), c(3)))
-  if (writePDF) dev.off()
+  if (writePDF){
+    g <- gridExtra::arrangeGrob(grobs = list(p1, p2, p3), ncol=1, heights = c(6,2,2))
+    ggsave(file = paste(meta_row$instrument_name, '-ggmap.pdf', sep=''), width=8, height=8, units = 'in', g)
+    #dev.off()
+    print(paste('Maps written to ', meta_row$instrument_name, '-ggmap.pdf.', sep=''))
+  } else{
+    gridExtra::grid.arrange(grobs = list(p1, p2, p3), ncol=1, heights = c(6,2,2))
+    print('Should have output plot to graphics device.')
+  }
 
 }
