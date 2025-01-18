ba49f53acd903538a845232462dc893c0dbd1e80
Camrin Braun <camrin.braun@gmail.com>
2025-01-18 16:14:19 -0500

    this interpolated timeseries step was causing issues so its removed for now

--- R/get_series.r
+++ R/get_series.r
@@ -59,7 +59,7 @@ get_series <- function(etuff, temp_res = NULL, what_tz = NULL){
   }
 
   ## merge depth/temp series data onto what full series would look like
-  if (class(etuff) != 'etuff_archival') series <- dplyr::left_join(dt_vec, series, by = "DateTime_local")
+  #if (class(etuff) != 'etuff_archival') series <- dplyr::left_join(series, dt_vec, by = "DateTime_local")
 
   return(series)
 }
